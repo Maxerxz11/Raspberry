25e1542c2bcc553175b86c33bad33c593d8f410f
Maxerxz11 <110257854+Maxerxz11@users.noreply.github.com>
2023-09-20 15:47:05 -0300

    Intento 2

--- pruebaLed.py
+++ pruebaLed.py
@@ -1,46 +1,14 @@
-import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BCM)
-RUNNING = True
+import timer 
+import Adafruit_DHT 
 
-green = 27
-red = 17
-blue = 22
+sencor = Adafruit_DHT.DHT22
+pin=23
 
-GPIO.setup(red, GPIO.OUT)
-GPIO.setup(green, GPIO.OUT)
-GPIO.setup(blue, GPIO.OUT)
+while True :
+	temperatura = Adafruit_DHT.read_retry(sencor,pin) #LO que hago aca es declarar una varible de temperatura para sacar analizar con el sencor la funcion read es para leer sus datos
 
-Freq = 100
-
-RED = GPIO.PWM(red, Freq)
-GREEN = GPIO.PWM(green, Freq)
-BLUE = GPIO.PWM(blue, Freq)
-
-try:
-	while RUNNING:
-	#lighting up the pins. 100 means giving 100% to the pin
-		RED.start(100)
-		GREEN.start(1)
-		BLUE.start(1)
-		#For anode RGB LED users, if you want to start with RED too the only thing to be done is defining RED as one and GREEN and BLUE as 100.
-		for x in range(1,101):
-		#for changing the width of PWM, this command is used
-			GREEN.ChangeDutyCycle(x)
-			#for anode LED users, just change x with 101-x
-			#and for delay time.sleep is used. You can chance the duration of the colors with changing the time from here
-			time.sleep(0.05)
-		for x in range(1,101):
-			RED.ChangeDutyCycle(101-x)
-			time.sleep(0.025)
-		for x in range(1,101):
-			GREEN.ChangeDutyCycle(101-x)
-			BLUE.ChangeDutyCycle(x)
-			time.sleep(0.025)
-		for x in range(1,101):
-			RED.ChangeDutyCycle(x)
-			time.sleep(0.025)
-except KeyboardInterrupt:
-#the purpose of this part is, when you interrupt the code, it will stop the while loop and turn off the pins, which means your LED won't light anymore
-	RUNNING = False
-	GPIO.cleanup()
+	if temperatura is not None:
+		print("La temperatura es {}".format(temperatura))
+	
+	else:
+		print("Error[-]")
