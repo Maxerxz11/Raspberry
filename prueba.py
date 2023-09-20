4e025cc7b36f5c9a9de5e3399053f2375ab32dfa
Maxerxz11 <maxerxz11@gmail.com>
2023-09-20 18:20:29 -0300

    Prueba6

--- prueba.py
+++ prueba.py
@@ -12,3 +12,9 @@ while True:
         print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
     else:
         print("Error[-]")
+
+except KeyboardInterrupt:
+    print('Lectura del sensor detenida por el usuario.')
+
+except Exception as e:
+    print(f'Error: {e}')
