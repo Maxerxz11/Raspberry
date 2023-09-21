42dc702019c64bc9d12e866a947e96cee9362129
Maxerxz11 <maxerxz11@gmail.com>
2023-09-21 15:42:56 -0300

    prueba15

--- prueba.py
+++ prueba.py
@@ -1,6 +1,6 @@
 import board
- import digitalio
- import busio print ( "¡Hola, flash!" ) # Intenta crear un 
+import digitalio
+import busio print ( "¡Hola, flash!" ) # Intenta crear un 
 pin de entrada digital = digitalio.DigitalInOut(board.D4) print ( "Digital IO ok!" ) # Intenta crear un dispositivo I2C 
 i2c = busio .I2C(board.SCL, board.SDA) print ( "¡I2C ok!" ) # Intente crear un dispositivo SPI 
 spi = busio.SPI(board.SCLK, board.MOSI, board.MISO) print ( "SPI ok! " ) imprimir ( "¡hecho!" )
