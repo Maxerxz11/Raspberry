993a2b2b0061a6085c1ae44a11c15c87cc29fb71
Maxerxz11 <maxerxz11@gmail.com>
2023-09-21 17:15:43 -0300

    pueba30

--- prueba.py
+++ prueba.py
@@ -1,17 +1,16 @@
 import time
-import board
 import adafruit_dht
+from board import pin
 
-
-dht_sensor = adafruit_dht.DHT22(24)
+dht_sensor = adafruit_dht.DHT22(18)
 
 while True:
     temperatura = dht_sensor.temperature  # Variables para leer la temperatura
+    print(temperatura)
+#    if temperatura is not None:
+#        print(f'Temperatura: {temperature:.2f}°C')
 
-    if temperatura is not None:
-        print(f'Temperatura: {temperature:.2f}°C')
-
-    else:
-        print("Error[-]")
+#    else:
+ #       print("Error[-]")
 
     time.sleep(1.0)
--- pruebaLed.py
+++ pruebaLed.py
@@ -1,14 +1,15 @@
-import timer 
-import Adafruit_DHT 
+import timer
+import Adafruit_DHT
 
 sencor = Adafruit_DHT.DHT22
-pin=23
+pin = 4
 
-while True :
-	temperatura = Adafruit_DHT.read_retry(sencor,pin) #LO que hago aca es declarar una varible de temperatura para sacar analizar con el sencor la funcion read es para leer sus datos
+while True:
+    # LO que hago aca es declarar una varible de temperatura para sacar analizar con el sencor la funcion read es para leer sus datos
+    humedad, temperatura = Adafruit_DHT.read_retry(sencor, pin)
 
-	if temperatura is not None:
-		print("La temperatura es {}".format(temperatura))
-	
-	else:
-		print("Error[-]")
+    if temperatura is not None:
+        print("La temperatura es {}".format(temperatura))
+
+    else:
+        print("Error[-]")
