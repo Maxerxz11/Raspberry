2f2833bbc72d50c7846a1fdeac58b3eedc840899
Maxerxz11 <maxerxz11@gmail.com>
2023-09-21 17:24:36 -0300

    prueba funcional

--- pruebaLed.py
+++ pruebaLed.py
@@ -1,15 +1,17 @@
 import time
 import Adafruit_DHT
 
-sencor = Adafruit_DHT.DHT22
+sensor = Adafruit_DHT.DHT22
 pin = 4
 
 while True:
     # LO que hago aca es declarar una varible de temperatura para sacar analizar con el sencor la funcion read es para leer sus datos
-    humedad, temperatura = Adafruit_DHT.read_retry(sencor, pin)
+    humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
 
     if temperatura is not None:
-        print("La temperatura es {}".format(temperatura))
+        print(temperatura)
+        print(humedad)
+        print("La temperatura es {}".format(temperatura, humedad))
 
     else:
         print("Error[-]")
