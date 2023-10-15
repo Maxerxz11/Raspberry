37e3d2014838f4354eafdc8312cca8ecacc5afb7
Maxerxz11 <maxerxz11@gmail.com>
2023-10-15 18:05:01 -0300

    funcional 15

--- prueba.py
+++ prueba.py
@@ -14,21 +14,18 @@ BLUE_PIN = 27  # Reemplaza con el número de pin físico para el azul
 
 # Configura los pines como salida
 GPIO.setup(RED_PIN, GPIO.OUT)
-GPIO.setup(GREEN_PIN, GPIO.OUT)
-GPIO.setup(BLUE_PIN, GPIO.OUT)
+
 
 # Enciende el LED en rojo
 GPIO.output(RED_PIN, GPIO.HIGH)
-GPIO.output(GREEN_PIN, GPIO.LOW)
-GPIO.output(BLUE_PIN, GPIO.LOW)
+
 
 # Espera unos segundos
 time.sleep(3)
 
 # Apaga el LED
 GPIO.output(RED_PIN, GPIO.LOW)
-GPIO.output(GREEN_PIN, GPIO.LOW)
-GPIO.output(BLUE_PIN, GPIO.LOW)
+
 
 # Limpia los pines GPIO
 GPIO.cleanup()
