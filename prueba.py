68cb71e708281c7430a2cb97f76e0a5dc9229dd9
Maxerxz11 <maxerxz11@gmail.com>
2023-10-15 18:03:04 -0300

    funcional 14

--- prueba.py
+++ prueba.py
@@ -1,13 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
-# Configura los pines GPIO
-RED_PIN = 17
-GREEN_PIN = 18
-BLUE_PIN = 27
+# Deshabilitar las advertencias
+GPIO.setwarnings(False)
 
-# Configura el modo de los pines
+# Configura los pines GPIO en modo BOARD
 GPIO.setmode(GPIO.BCM)
+
+# Define los números de pin físicos
+RED_PIN = 17  # Reemplaza con el número de pin físico para el rojo
+GREEN_PIN = 18  # Reemplaza con el número de pin físico para el verde
+BLUE_PIN = 27  # Reemplaza con el número de pin físico para el azul
+
+# Configura los pines como salida
 GPIO.setup(RED_PIN, GPIO.OUT)
 GPIO.setup(GREEN_PIN, GPIO.OUT)
 GPIO.setup(BLUE_PIN, GPIO.OUT)
