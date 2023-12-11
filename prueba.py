0c313bae39512cdd3490a7f71149b15cdb834382
Maxerxz11 <maxerxz11@gmail.com>
2023-12-11 16:39:15 -0300

    cambio 2

--- prueba.py
+++ prueba.py
@@ -1,17 +1,8 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-# Configuración del sensor
-# Ajusta los pines según tu conexión
-sensor = DistanceSensor(echo=23, trigger=24)
+sensor = DistanceSensor(echo=17, trigger=4)
 
-try:
-    while True:
-        distancia = sensor.distance * 100  # Convertir a centímetros
-        print(f'Distancia: {distancia:.2f} cm')
-        sleep(1)
-
-except KeyboardInterrupt:
-    print("Programa interrumpido por el usuario")
-finally:
-    pass
+while True:
+    print(f'Distancia: {sensor.distance * 100:.2f} cm')
+    sleep(1)
